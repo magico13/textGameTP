21d68b1a5c16e7c8d9b78de2396a5a3addf5f80b
Caleb Gaffney <ioicaleb12@gmail.com>
2021-09-29 08:41:37 -0400

    Added player, combat, and enemies to text game, Map broken

---
+++ Text Adventure Game/TextAdventureGame/TextAdventureGame/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAdventureGame
+{
+    public class Enemy : Character
+    {
+        public Enemy(string name) : base(name)
+        {
+            Health = 10;
+        }
+    }
+}
---
+++ Text Adventure Game/TextAdventureGame/TextAdventureGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventureGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool gameOver = false;
+            Location map = new Location();
+            map.CreateMap();
+            string gameTitle = "Quest to the Center of a Tootsie Pop";
+            Console.WriteLine(gameTitle);
+            Console.WriteLine("How many licks does it take to get to the center of a Tootsie Pop?");
+            Console.WriteLine("The world may never know...");
+            Console.WriteLine();
+            Console.Write("Please enter a name for your character: ");
+            string input = Console.ReadLine();
+            Player player = new Player(input);
+            Console.WriteLine();
+            Console.WriteLine($"Welcome to the world, {player.Name}!");
+            Console.WriteLine();
+            while (!gameOver)
+            {
+                player.Prompt();
+            }
+            Console.WriteLine("Congratulations! You have answered the age old question!");
+        }
+        
+    }
+}
