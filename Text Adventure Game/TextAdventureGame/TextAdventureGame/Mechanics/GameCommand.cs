629ad431eb2e8c0ee47f6da062f3ce38ccf5a9d8
Caleb Gaffney <ioicaleb12@gmail.com>
2021-10-05 16:50:09 -0400

    Added objects class to be implemented further later

--- Text Adventure Game/TextAdventureGame/TextAdventureGame/Mechanics/GameCommand.cs
+++ Text Adventure Game/TextAdventureGame/TextAdventureGame/Mechanics/GameCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using TextAdventureGame.Characters;
 using TextAdventureGame.MapLocations;
+using TextAdventureGame.Objects;
 
 namespace TextAdventureGame.Mechanics
 {
@@ -17,6 +18,7 @@ namespace TextAdventureGame.Mechanics
         public bool InCombat { get; set; }
         public int Licks { get; set; } = 0;
         public Dialogue Dialogue { get; set; }
+        public Items Objects { get; set; }
 
         /// <summary>
         /// Handles all methods (input)
@@ -29,11 +31,12 @@ namespace TextAdventureGame.Mechanics
         /// (7) Damages enemy and handles end of battle if enemy or player dies
         /// </summary>
         /// <param name="input"></param>
-        public virtual void Execute(int input) 
+        public virtual void Execute(int input)
         {
             switch (input)
             {
                 case 1:
+                    PrintLine(Objects.CheckItem(Target));
                     break;
                 case 2: //Creates a player using user input as a name
                     string name = Prompt.GetName();
@@ -80,7 +83,7 @@ namespace TextAdventureGame.Mechanics
                         PrintLine(Player.EatCandy()); //Writes end of battle text including sugar level and experience
                         Console.WriteLine();
                     }
-                    else 
+                    else
                     {
                         Execute(3);
                     }
@@ -89,7 +92,7 @@ namespace TextAdventureGame.Mechanics
                     Execute(3);
                     break;
             }
-            
+
         }
 
         public void CreateCommands()
@@ -100,11 +103,12 @@ namespace TextAdventureGame.Mechanics
             Map = new Location();
             Combat = new Combat();
             Dialogue = new Dialogue();
+            Objects = new Items();
         }
 
         public void ActOnInput(string command, string target)
         {
-            
+
             switch (command)
             {
                 case "move": //checks input for location input
@@ -126,21 +130,21 @@ namespace TextAdventureGame.Mechanics
                     {
                         InCombat = false;
                     }
-                        string location;
-                        switch (target)
-                        {
-                            case "":
-                                location = Prompt.GetLocation();
-                                break;
-                            case null:
-                                location = Prompt.GetLocation();
-                                break;
-                            default:
-                                location = target;
-                                break;
-                        };
-                        Target = location;
-                        Execute(6); //Executes movement commands
+                    string location;
+                    switch (target)
+                    {
+                        case "":
+                            location = Prompt.GetLocation();
+                            break;
+                        case null:
+                            location = Prompt.GetLocation();
+                            break;
+                        default:
+                            location = target;
+                            break;
+                    };
+                    Target = location;
+                    Execute(6); //Executes movement commands
                     break;
 
                 case "lick": //Writes a lone of not in combat or starts damage process
@@ -172,7 +176,12 @@ namespace TextAdventureGame.Mechanics
                         PrintLine("Bye! Thanks for playing!");
                         Environment.Exit(0);
                     }
-                        break;
+                    break;
+
+                case "check":
+                    Target = target;
+                    Execute(1);
+                    break;
 
                 default: //Handles unrecognized inputs
                     PrintLine("Sorry. Please try again.");
@@ -204,6 +213,7 @@ namespace TextAdventureGame.Mechanics
             helpDetails["Move"] = "All done in this room? This will take you to another room.";
             helpDetails["Lick"] = "The only way to get to the center is to erode the candy coating. Get licking!";
             helpDetails["Rooms"] = "This house is so big that it's easy to get lost in. Use this to remember which rooms you can go to.";
+            helpDetails["Check"] = "You remember that thing you picked up earlier? It might be useful. Use this to look it over one more time";
             helpDetails["Help"] = "This... Well if you need an explanation, it lists the commands that will actually do things";
             helpDetails["Quit"] = "Yeah, so this game doesn't have an ending yet. Use this if you want to stop playing for now.";
 
---
+++ Text Adventure Game/TextAdventureGame/TextAdventureGame/Objects/Items.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TextAdventureGame.Mechanics;
+
+namespace TextAdventureGame.Objects
+{
+    public class Items : GameCommand
+    {
+        public Items()
+        {
+            Inventory = new List<Items>();
+        }
+        public string Name { get; }
+        public string Description { get; }
+        public List<Items> Inventory { get; set; }
+
+        /// <summary>
+        /// Returns the description of an item
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public string CheckItem(string target)
+        {
+            foreach (Items item in Inventory)
+            {
+                if (item.Name == target)
+                {
+                    return item.Description;  
+                }
+            }
+            return $"You don't have a {target}";
+        }
+
+        public void CheckInventory()
+        {
+            foreach (Items item in Inventory)
+            {
+                PrintLine(item.Name);
+            }
+        }
+    }
+}
