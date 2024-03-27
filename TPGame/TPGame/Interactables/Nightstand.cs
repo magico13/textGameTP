c6d0dfc038bdab9b828691e89eb7164e8ccb6ed1
Caleb Gaffney <ioicaleb12@outlook.com>
2024-03-27 09:44:04 -0500

    Add name, description, and function to nightstand, rename guard

--- TPGame/TPGame/Interactables/Nightstand.cs
+++ TPGame/TPGame/Interactables/Nightstand.cs
@@ -1,8 +1,19 @@
-﻿using TPGame.Models;
+﻿using TPGame.Handlers;
+using TPGame.Models;
 
 namespace TPGame.Interactables
 {
     public class Nightstand : Interactable
     {
+        public Nightstand()
+        {
+            Name = "Nightstand";
+            Description = "A small cherry nightstand with a reading lamp on top. The bulb has burned out, which you make a note to replace, intending to actually follow through this time.";
+        }
+
+        public override void UseInteractable()
+        {
+            DialogueHandler.PrintLine("You open the draw to discover a familiar case. It's your mouth GUARD that you left behind when this nightstand used to be in your room. It should still fit your mouth.");
+        }
     }
 }
